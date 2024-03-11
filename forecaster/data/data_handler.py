72ca96b9104105b664fafa91ea6160b4bff988d5
egpivo <egpivo@gmail.com>
2024-03-11 19:03:03 +0800

    CHG: add pre-commit and refine the code

--- forecaster/data/data_handler.py
+++ forecaster/data/data_handler.py
@@ -12,7 +12,9 @@ class DataHandler:
     0	3cf2d95c-851a-3e66-bd62-36050c1aa8dd	M	30.0
     """
 
-    def __init__(self, user_data_path: str, transaction_data_path: str, store_data_path: str) -> None:
+    def __init__(
+        self, user_data_path: str, transaction_data_path: str, store_data_path: str
+    ) -> None:
         self.user_data_path = user_data_path
         self.transaction_data_path = transaction_data_path
         self.store_data_path = store_data_path
@@ -23,24 +25,32 @@ class DataHandler:
         -----
         - Will remove missing data in this work, which can be enhanced later
         """
-        default_columns = ['id', 'gender', 'age']
+        default_columns = ["id", "gender", "age"]
         user_pdf = pd.read_csv(self.user_data_path)
 
-        assert any(user_pdf.columns == default_columns), f"Check the input columns of users, got {user_pdf.columns}"
+        assert any(
+            user_pdf.columns == default_columns
+        ), f"Check the input columns of users, got {user_pdf.columns}"
         return user_pdf.rename({"id": "user_id"}, axis=1).dropna()
 
     def fetch_transaction_data(self) -> pd.DataFrame:
         """Return: 'id', 'user_id', 'store_id', 'event_occurrence', 'amount'"""
-        default_columns = ['id', 'user_id', 'store_id', 'event_occurrence', 'amount']
-        transaction_pdf = pd.read_csv(self.transaction_data_path, parse_dates=["event_occurrence"])
+        default_columns = ["id", "user_id", "store_id", "event_occurrence", "amount"]
+        transaction_pdf = pd.read_csv(
+            self.transaction_data_path, parse_dates=["event_occurrence"]
+        )
 
-        assert any(transaction_pdf.columns == default_columns), f"Check the input columns of transaction, got {transaction_pdf.columns}"
+        assert any(
+            transaction_pdf.columns == default_columns
+        ), f"Check the input columns of transaction, got {transaction_pdf.columns}"
         return transaction_pdf
 
     def fetch_store_data(self) -> pd.DataFrame:
         """Return: 'user_id', 'gender', 'age'"""
-        default_columns = ['id', 'nam', 'laa', 'category', 'lat', 'lon']
+        default_columns = ["id", "nam", "laa", "category", "lat", "lon"]
         store_pdf = pd.read_csv(self.store_data_path)
 
-        assert any(store_pdf.columns == default_columns), f"Check the input columns of stores, got {store_pdf.columns}"
+        assert any(
+            store_pdf.columns == default_columns
+        ), f"Check the input columns of stores, got {store_pdf.columns}"
         return store_pdf.rename({"id": "store_id"}, axis=1)
--- forecaster/data/utils.py
+++ forecaster/data/utils.py
@@ -1,7 +1,9 @@
 import pandas as pd
-from typing import Union, Tuple
 
-def generate_negative_samples(full_data_pdf: pd.DataFrame, num_negative_samples: int = 5, random_state: int = 42) -> pd.DataFrame:
+
+def generate_negative_samples(
+    full_data_pdf: pd.DataFrame, num_negative_samples: int = 5, random_state: int = 42
+) -> pd.DataFrame:
     """
     Generate negative samples for a binary classification problem.
 
@@ -22,31 +24,43 @@ def generate_negative_samples(full_data_pdf: pd.DataFrame, num_negative_samples:
     """
 
     # Extract unique user and store pairs from positive instances
-    positive_pairs = full_data_pdf[['user_id', 'store_id']].drop_duplicates()
+    positive_pairs = full_data_pdf[["user_id", "store_id"]].drop_duplicates()
 
     # Generate a list of all possible user and store pairs
     all_user_store_pairs = pd.DataFrame(
-        index=pd.MultiIndex.from_product([full_data_pdf['user_id'].unique(), full_data_pdf['store_id'].unique()],
-                                         names=['user_id', 'store_id'])).reset_index()
+        index=pd.MultiIndex.from_product(
+            [full_data_pdf["user_id"].unique(), full_data_pdf["store_id"].unique()],
+            names=["user_id", "store_id"],
+        )
+    ).reset_index()
 
     # Identify negative pairs by finding those not present in positive pairs
-    negative_pairs = all_user_store_pairs[~all_user_store_pairs.set_index(['user_id', 'store_id']).index.isin(positive_pairs.set_index(['user_id', 'store_id']).index)]
+    negative_pairs = all_user_store_pairs[
+        ~all_user_store_pairs.set_index(["user_id", "store_id"]).index.isin(
+            positive_pairs.set_index(["user_id", "store_id"]).index
+        )
+    ]
 
     # Randomly sample negative stores for each user
-    users = negative_pairs['user_id'].unique()
-    negative_samples = pd.DataFrame(columns=['user_id', 'store_id', 'label'])
+    users = negative_pairs["user_id"].unique()
+    negative_samples = pd.DataFrame(columns=["user_id", "store_id", "label"])
 
     for user in users:
         # Randomly sample negative stores not present in positive instances for each user
-        negative_stores = negative_pairs[negative_pairs['user_id'] == user].sample(n=num_negative_samples, random_state=random_state)
+        negative_stores = negative_pairs[negative_pairs["user_id"] == user].sample(
+            n=num_negative_samples, random_state=random_state
+        )
         negative_samples = pd.concat([negative_samples, negative_stores])
 
     # Add a 'label' column indicating negative instances
-    negative_samples['label'] = 0
+    negative_samples["label"] = 0
     # Combine positive and negative samples
-    balanced_dataset = pd.concat([full_data_pdf[['user_id', 'store_id', 'label']], negative_samples])
+    balanced_dataset = pd.concat(
+        [full_data_pdf[["user_id", "store_id", "label"]], negative_samples]
+    )
     # Shuffle the dataset to mix positive and negative samples
-    balanced_dataset = balanced_dataset.sample(frac=1, random_state=random_state).reset_index(drop=True)
+    balanced_dataset = balanced_dataset.sample(
+        frac=1, random_state=random_state
+    ).reset_index(drop=True)
 
     return balanced_dataset
-
